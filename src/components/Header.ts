9028d15a48d5a5adf00692d7e8f1cb0c4ebb5850
Mohamed Oulahguine <mohamedoulahguine@gmail.com>
2025-06-06 19:09:56 +0300

    refix respon for form search

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,59 +1,38 @@
-const searchForm = document.querySelector<HTMLFormElement>("#search_form");
-const searchInputIcon =
-  document.querySelector<HTMLElement>("#search_input_icon");
+const searchForm: any = document.querySelector<HTMLFormElement>("#search_form");
+const searchInputIcon: any = document.querySelector<HTMLElement>(
+  ".search_btn_sscreen i"
+);
 const searchInput = document.querySelector<HTMLInputElement>("#search_input");
 const searchButton = document.querySelector<HTMLButtonElement>("#search_btn");
 
 const isSmallScreen = () => window.innerWidth <= 768;
 
-if (searchForm && searchInputIcon && searchInput && searchButton) {
-  searchInputIcon.addEventListener("click", () => {
-    if (isSmallScreen()) {
-      searchInput.style.display = "block";
-      searchButton.style.display = "block";
-      searchInput.focus();
+searchInputIcon.addEventListener("click", () => {
+  searchForm.style.display = "flex";
+});
 
-      Object.assign(searchForm.style, {
-        backgroundColor: "white",
-        width: "300px",
-        position: "absolute",
-        right: "250px",
-        zIndex: "15",
-        border: "1px solid #eee",
-      });
-    }
-  });
-
-  searchForm.addEventListener(
-    "focusout",
-    (e) => {
-      const relatedTarget = e.relatedTarget as HTMLElement | null;
-      if (!searchForm.contains(relatedTarget)) {
-        if (isSmallScreen()) {
-          searchInput.style.display = "";
-          searchButton.style.display = "";
-          Object.assign(searchForm.style, {
-            backgroundColor: "white",
-            width: "300px",
-            position: "absolute",
-            right: "-10px",
-            zIndex: "-1",
-            border: "0",
-          });
-        }
-      }
-    },
-    true
-  );
-  document.addEventListener("click", (e) => {
-    if (
-      isSmallScreen() &&
-      searchForm &&
-      !searchForm.contains(e.target as Node)
-    ) {
-      searchInput.style.display = "";
-      searchButton.style.display = "";
-      searchForm.removeAttribute("style");
-    }
-  });
-}
+// if (searchForm && searchInputIcon) {
+//   searchForm.addEventListener(
+//     "focusout",
+//     (e) => {
+//       const relatedTarget = e.relatedTarget as HTMLElement | null;
+//       if (!searchForm.contains(relatedTarget)) {
+//         if (isSmallScreen()) {
+//           Object.assign(searchForm.style, {
+//             display: "block",
+//           });
+//         }
+//       }
+//     },
+//     true
+//   );
+//   document.addEventListener("click", (e) => {
+//     if (
+//       isSmallScreen() &&
+//       searchForm &&
+//       !searchForm.contains(e.target as Node)
+//     ) {
+//       searchForm.removeAttribute("style");
+//     }
+//   });
+// }
