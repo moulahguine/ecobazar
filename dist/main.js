c2b5bf860c9bfb24666d5b9a6216588f84583d86
Mohamed Oulahguine <mohamedoulahguine@gmail.com>
2025-06-14 15:18:11 +0300

    Use the TypeScript compiler (tsc) to compile all .ts files in your project into .js

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-require("./pages/About");
+require("./styles/main.scss");
+require("./components/Header");
+require("./components/nav");
+require("./pages/hero");
---
+++ dist/pages/hero.js
@@ -0,0 +1,17 @@
+"use strict";
+const heroImages = document.querySelector(".hero_images");
+if (heroImages) {
+    let scrollPosition = 0;
+    setInterval(() => {
+        if (!heroImages)
+            return;
+        scrollPosition += heroImages.clientWidth;
+        if (scrollPosition >= heroImages.scrollWidth) {
+            scrollPosition = 0;
+        }
+        heroImages.scrollTo({
+            left: scrollPosition,
+            behavior: "smooth",
+        });
+    }, 5000);
+}
